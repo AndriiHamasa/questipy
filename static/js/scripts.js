cb1de9603cf136089c3ed2d9ec9b6742eda6968b
Andrii <andrii.khamaza0@gmail.com>
2024-09-22 01:53:01 +0300

    chenged main nav in header

---
+++ static/js/scripts.js
@@ -0,0 +1,11 @@
+document.getElementById('customSelect').addEventListener('click', function() {
+    this.classList.toggle('active');
+});
+
+// Закрытие выпадающего списка при клике вне
+document.addEventListener('click', function(event) {
+    const select = document.getElementById('customSelect');
+    if (!select.contains(event.target)) {
+        select.classList.remove('active');
+    }
+});
