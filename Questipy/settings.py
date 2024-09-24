a423ba9c80f7a615df3b9ce8cb63f4da0ac3a805
Andrii <andrii.khamaza0@gmail.com>
2024-09-24 22:09:39 +0300

    add secret key to .env file

--- Questipy/settings.py
+++ Questipy/settings.py
@@ -24,7 +24,7 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-7%1u#4v5qd9-lakqy!*x%r%n407n#ly&380cp_o6$v6$gfgrm2"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
