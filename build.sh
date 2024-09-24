58f8d1434c32bd9dab733633dc3d3ff07958514f
Andrii <andrii.khamaza0@gmail.com>
2024-09-24 22:57:04 +0300

    add load fixtures to build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
-#Exit on Error
+# Exit on Error
 set -o errexit
 
-#Modify this line as needed for your package manager (pip, poetry, etc.)
+# Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-#Convert static asset files
+# Convert static asset files
 python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+# Load fixtures
+python manage.py loaddata catalog_fixture.json
